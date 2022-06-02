280871cd9d2c26840086c082e99091d96a2d2790
Gerald Freislich <freislichg@gmail.com>
2022-06-03 08:30:40 +0930

    safe

---
+++ unit_tests/testing_physics_of_particle_drop.py
@@ -0,0 +1,17 @@
+import path
+import sys
+  
+# directory reach
+directory = path.Path(__file__).abspath()
+  
+# setting path
+sys.path.append(directory.parent.parent)
+  
+from shapes import Shape
+import pygame as py
+from physics_simulations import Physics_Simulations
+from particle import Particle
+from particle_movements.py import Particle_movements
+
+# testing the particle movements
+
