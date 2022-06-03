cfc35ff05758934d3442901ba228f296892cc224
Gerald Freislich <freislichg@gmail.com>
2022-06-03 14:12:59 +0930

    testing

--- particle_movements.py
+++ particle_movements.py
@@ -27,37 +27,25 @@ class Particle_Movement():
         self.__under_particle = last_particle
     
     # returning the particles (self and current as a tuple)
-    def get_particles(self)->Particle:
+    def get_particle(self)->Particle:
         return self.__current_particle
     
     # calculate the positions of the particles based on the movement and direction of the previous particle
     def calc_positions(self):
-        # checking what the last x and y positions were, and if they had a greater flux than 20
-        # flux will be a measure of element [0] to the current element:
-        # the position of the current element will be given by previous_element (x,y) + (5, 0) {if element is on left} and (x,y) - (5, 0) {if element is on right}
-        # lowest position should be constant x = 250
-        current_x = self.__current_particle.get_points()[0]; last_x = self.__current_particle.get_points()[1]
-        if(current_x < 300):
-            current_x += 5
-        else:
-            self.__reverse_flux = True
-        print("here")
+        # getting the x positions of the current particle and the particle above
+        current_x = self.__current_particle.get_points()[0]; last_x = self.__current_particle.get_points()[0]
+        if(self.__reverse_flux is not True): # if we are moving each particle inward
+            if(current_x < 300): # if the particle is not past the central point (where flux is at max (or g'(t) = 0 && f'(t) != 0){if treated as parametric})
+                current_x += 5
+            else: # if we are past the central x position we must reverse
+                self.__reverse_flux = True # reversing the direction
+        else: # if we need to move the items backwards
+            if(current_x > 250): # if we are greater than the minimal x position
+                current_x -= 5
+            else:
+                self.__reverse_flux = False
 
+        # updating the current particles position
+        self.__current_particle.move_particle(current_x, self.__current_particle.get_points()[1]);
 
-"""Planning the positions calculator
-What formulas to use:
--> As the first ball hits the ground there is a net loss and transfer of momentum in the negative direction (i.e transfer of momentum backwards / upwards)
-In doing so the ball above it must also transfer its momentum, however, this collision is again not in an isolated environment so not all of the energy
--> (Ke) will be conserved. Likewise, this will again happen to the one above.
----> This will continue upwards, however, this is not the concern, but the concern is that the last ball (that transfers its energy) will actually rebound
---> If the balls are dead on it will rebound straight down. But, if the particles represent a material they can never truly be vertically aligned (well they can)
---> (They just have to represent a laticed material or hydrocarbon chain)
-"""
-
-"""
-Drop stages:
--> box drops
--> hits floor and bounces upwards at 1/2 height
-    -> when hit floor box condenses and sides slightly cave, dependent on material flex:
-        -> biggest cave in centre of side (side, len / 2 {element at this point})
-"""
+    def get_flux()->bool: return self.__reverse_flux
--- unit_tests/testing_physics_of_particle_drop.py
+++ unit_tests/testing_physics_of_particle_drop.py
@@ -11,7 +11,16 @@ from shapes import Shape
 import pygame as py
 from physics_simulations import Physics_Simulations
 from particle import Particle
-from particle_movements.py import Particle_movements
+from particle_movements import Particle_Movement
 
-# testing the particle movements
+# testing the particle movements, specifically the horizontal flux of the particles to simulate the contraction and expansion of a soft body
 
+# particles: current and past to resemble the node like behaviour of the material
+current_particle = Particle(10, 250, 250); last_particle = Particle(10, 250, 240)
+print(current_particle.get_points()[0])
+# instantiating the Particle movement class
+particle_mover = Particle_Movement(current_particle, last_particle)
+particle_mover.calc_positions()
+
+current_particle = particle_mover.get_particle();
+print(current_particle.get_points()[0])
