e78476e20203c73f06c06075c661361375f0a8ec
Gerald Freislich <freislichg@gmail.com>
2022-06-05 00:23:15 +0930

    safe

--- physics_simulations.py
+++ physics_simulations.py
@@ -51,6 +51,7 @@ class Physics_Simulations():
                 pos = tuple(self.__object_array[i].get_points())
                 self.__object_array[i].move_particle(pos[0], pos[1]+self.__momentum)
             self.__momentum += 2
+
         elif(self.__momentum > 0 and self.__momentum < 30):
             for i in range(0, len(self.__object_array)):
                 pos = tuple(self.__object_array[i].get_points())
