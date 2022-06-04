b38aa115c8d24945c0d0a35bff71c4291f72d6b0
Gerald Freislich <freislichg@gmail.com>
2022-06-05 00:19:20 +0930

    safe

--- physics_simulations.py
+++ physics_simulations.py
@@ -32,7 +32,7 @@ class Physics_Simulations():
         self.__simulation_type = None
         self.__drop_stage = 0
         self.__drop_momentum_eq = "(-3x**4) - (1/9)(x**3) + 9(x**2) - x - 4"
-        self.__momentum = 17
+        self.__momentum = 250
 
     
     def set_attrs(self, window, simulation_type: str, object_array: list)->None:
@@ -50,7 +50,7 @@ class Physics_Simulations():
             for i in range(0, len(self.__object_array)):
                 pos = tuple(self.__object_array[i].get_points())
                 self.__object_array[i].move_particle(pos[0], pos[1]+self.__momentum)
-            self.__momentum -= 1
+            self.__momentum -= 17
 
     def rebound_square(self, object_array:list)->None:
         if(self.__object_array[0].get_points()[1] > 350 and self.__drop_stage == 2):
