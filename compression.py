c5c9ec7440ef5bbf967ac70e7e49784292747945
Gerald Freislich <freislichg@gmail.com>
2022-06-18 23:10:57 +0930

    safe

--- compression.py
+++ compression.py
@@ -29,13 +29,13 @@ class Compression():
     # function to expand the shape after compression
     def expansion_behaviour(self, momentum: int, i: int):
         for obj in self.__object_array[i:9-i+1]: # problem (for every iteration we move left and down)
-            obj.move_particle(obj.get_points()[0]+7, obj.get_points()[1]+momentum)
+            obj.move_particle(obj.get_points()[0]+6, obj.get_points()[1]+momentum)
         
         for obj in self.__object_array[10-i:20+i]: # problem (for every iteration we move left and down)
             obj.move_particle(obj.get_points()[0], obj.get_points()[1]+momentum)
 
         for obj in self.__object_array[21+i:29-i+1]: # problem (for every iteration we move left and down)
-            obj.move_particle(obj.get_points()[0]-7, obj.get_points()[1]+momentum)
+            obj.move_particle(obj.get_points()[0]-8, obj.get_points()[1]+momentum)
         return self.__object_array
     
 
