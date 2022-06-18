4f954428b52bf214f9abc0c94e0de04611b2600b
Gerald Freislich <freislichg@gmail.com>
2022-06-18 22:34:05 +0930

    safe

--- unit_tests/compression_tests.py
+++ unit_tests/compression_tests.py
@@ -14,7 +14,26 @@ from particle import Particle
 from compression import Compression
 
 def tests():
+    shape_obj = Shape()
+    shape_obj.make_shape_array_square() # making the shape array
+    shape_array = shape_obj.get_shape_array()
     comp = Compression()
     simulation = Physics_Simulations()
     simulation.drop_square(shape_array)
     shape_array = simulation.get_shape_array()
+    
+    for i in range(len(shape_array)):
+        print(shape_array[i].get_points()[0], shape_array[i].get_points()[1], i)
+    
+    print("\n")
+
+    comp.set_object_array(shape_array)
+    for i in range(9):
+        shape_array = comp.compression_behaviour(5, i)
+        comp.set_object_array(shape_array)
+
+    for i in range(len(shape_array)):
+        print(shape_array[i].get_points()[0], shape_array[i].get_points()[1], i)
+    
+
+tests()
