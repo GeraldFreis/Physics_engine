619d9373dfa1cdc17fd0400aa8b7cf2a23710dc9
Gerald Freislich <freislichg@gmail.com>
2022-06-18 22:13:44 +0930

    safe

---
+++ compression.py
@@ -0,0 +1,49 @@
+"""
+Compression class:
+* This class will handle the falling and compression behaviours of the square
+"""
+class Compression():
+    __slots__ = ("__object_array", "__compressed_particles")
+    def __init__(self):
+        self.__object_array = list()
+        self.__compressed_particles = list()
+    
+    def set_object_array(self, given_array: list):
+        self.__object_array = given_array;
+
+    """
+    Complexity with this behaviour is that we must incrementally move all components / particles outward
+    """
+    def compression_behaviour(self, momentum: int, i: int):
+        for obj in self.__object_array[i:9-i]: # problem (for every iteration we move left and down)
+            obj.move_particle(obj.get_points()[0]-5, obj.get_points()[1]+momentum)
+        
+        for obj in self.__object_array[10-i:20+i]: # problem (for every iteration we move left and down)
+            obj.move_particle(obj.get_points()[0], obj.get_points()[1]+momentum)
+
+        for obj in self.__object_array[21+i:29-i]: # problem (for every iteration we move left and down)
+            obj.move_particle(obj.get_points()[0]+5, obj.get_points()[1]+momentum)
+        return self.__object_array
+    
+
+
+"""
+Plan:
+How to move the particles incrementally outwards and down
+(We can use indexing, i.e take particles 1->9 and 21->30 and slowly move them outwards):
+-> Use case:
+    - The bottom of the shape hits 450:
+    - The top continues to move down as the sides press out
+    - For every iteration the remaining particles move outwards:
+        - Remaining means those that are greater than the last particle indexes added to the index list of particles moved
+    - We will stop this when there are 4 particles left:
+    - for instance, the particles on the left of the shape will move out:
+        - index 1 and 9 will join the bottom and top line of the shape respectively
+        - index 2 and 8 will again join the bottom and top of the shape for the second iteration
+        - index 3 and 7 will join the bottom and top and then we reverse
+        - index 3 and 7 will return back to the side, as the top moves up
+        - index 2 and 8 will join the side again,
+        - index 1 and 9 will join the shape once more
+- We will use an index array to keep track of which particles
+
+"""
--- physics_simulations.py
+++ physics_simulations.py
@@ -1,7 +1,7 @@
 import pygame as py
 
 from particle import Particle  # the individual particles on screen
-
+from compression import Compression # the compression behaviours of the square
 """
 Class to handle the simulations of the engine (i.e dropping and react to player movements)
 
@@ -18,14 +18,8 @@ has methods:
         - drops the square until one of the lowest particles has reached height 450
 """
 class Physics_Simulations():
-    __slots__ = ("__window", "__object_array","__simulation_type", "__drop_stage", "__drop_momentum_eq", "__momentum", "__top_offset")
-    # __window = None
-    # __object_array = None
-    # __simulation_type = None
-    # __drop_momentum_eq = None 
-    # __drop_stage = None
-    # __momentum = None
-    # __top_offset = None
+    __slots__ = ("__window", "__object_array","__simulation_type", "__drop_stage", "__drop_momentum_eq", "__momentum", "__top_offset",
+    "__index_val", "under_compression", "compressed_object")
 
     def __init__(self):
         self.__object_array = list()
@@ -34,7 +28,9 @@ class Physics_Simulations():
         self.__drop_stage = 0
         self.__drop_momentum_eq = "(-3x**4) - (1/9)(x**3) + 9(x**2) - x - 4"
         self.__momentum = 0
-
+        self.__index_val = 0; # value that will hold the index of the current element which is increased every iteration
+        self.under_compression = True
+        self.compressed_object = Compression()
     
     def set_attrs(self, window, simulation_type: str, object_array: list)->None:
         self.__object_array = object_array
@@ -53,8 +49,15 @@ class Physics_Simulations():
                     self.__object_array[i].move_particle(pos[0], pos[1]+self.__momentum)    
                 self.__momentum += 5 / 200
             else:
-                self.__drop_stage += 1
-                self.__momentum = -1.5
+                self.compressed_object.set_object_array(self.__object_array)
+                if(self.under_compression is True):
+                    self.__object_array = self.compressed_object.compression_behaviour(self.__momentum, self.__index_val)
+                    self.__index_val += 1
+                    if(self.__index_val > 9):
+                        self.under_compression = False
+                else:
+                    self.__drop_stage += 1
+                    self.__momentum = -1.5
 
         elif(self.__drop_stage == 1):    # second stage (rebound) of the drop
             if(self.__object_array[0].get_points()[1] >= 390 and self.__object_array[0].get_points()[1] < 460):
